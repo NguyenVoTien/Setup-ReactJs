f44f5e02e847063c298337a8c216a23297f0fd07
NguyenVoTien <tienvo28052000@gmail.com>
2023-09-27 18:19:32 +0700

    #2 Hook with useEffect

---
+++ src/imports/Import.jsx
@@ -0,0 +1,3 @@
+// Imports useEffect
+export { default as Bai1UseEffect } from '../component/useEffect/exercise1/index.jsx';
+export { default as Bai2UseEffect } from '../component/useEffect/exercise2/scroll.jsx';
